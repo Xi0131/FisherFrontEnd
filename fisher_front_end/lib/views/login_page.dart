c335524720236af5d6d53b0ccf78411741c34f85
Kiki768 <114562955+Kiki768@users.noreply.github.com>
2024-12-02 17:30:50 +0800

    Update login_page.dart

--- fisher_front_end/lib/views/login_page.dart
+++ fisher_front_end/lib/views/login_page.dart
@@ -17,9 +17,9 @@ class _LoginPageState extends State<LoginPage> {
       backgroundColor: const Color(0xFFF5F5F5), // 背景顏色
       child: Center(
         child: Container(
-          padding: const EdgeInsets.all(40), // padding: 與邊邊留白的距離
-          width: 1000, // 邊框長度
-          height: 750,
+          padding: const EdgeInsets.all(20), // padding: 與邊邊留白的距離
+          width: 500, // 邊框長度
+          height: 500,
 
           decoration: BoxDecoration(
             color: CupertinoColors.white,
@@ -39,12 +39,12 @@ class _LoginPageState extends State<LoginPage> {
                 // SIGN IN
                 'SIGN IN',
                 style: TextStyle(
-                  fontSize: 100,
+                  fontSize: 80,
                   fontWeight: FontWeight.bold,
                   color: CupertinoColors.black,
                 ),
               ),
-              const SizedBox(height: 45),
+              const SizedBox(height: 20),
               // Captain 和 Crew 選擇按鈕
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -61,8 +61,8 @@ class _LoginPageState extends State<LoginPage> {
                     child: Container(
                       padding: const EdgeInsets.symmetric(
                         // 按鈕內的邊距（上下 15 像素，左右 25 像素），使文字有足夠的空間
-                        vertical: 25,
-                        horizontal: 50,
+                        vertical: 15,
+                        horizontal: 20,
                       ),
                       decoration: BoxDecoration(
                         // 控制外觀
@@ -78,7 +78,7 @@ class _LoginPageState extends State<LoginPage> {
                       child: Text(
                         'Captain',
                         style: TextStyle(
-                          fontSize: 50, // 增加字體大小
+                          fontSize: 30, // 增加字體大小
                           fontWeight: FontWeight.bold, //文字加粗
                           color: selectedRole ==
                                   'Captain' //如果是選captain的話，文字為白色，否則為黑色
@@ -95,7 +95,7 @@ class _LoginPageState extends State<LoginPage> {
                     padding: EdgeInsets.zero,
                     onPressed: () {
                       setState(() {
-                        selectedRole = 'Crew';
+                        selectedRole = 'Worker';
                       });
                     },
                     //onTap: () {
@@ -105,11 +105,11 @@ class _LoginPageState extends State<LoginPage> {
                     //},
                     child: Container(
                       padding: const EdgeInsets.symmetric(
-                        vertical: 25,
-                        horizontal: 75,
+                        vertical: 15,
+                        horizontal: 25,
                       ),
                       decoration: BoxDecoration(
-                        color: selectedRole == 'Crew'
+                        color: selectedRole == 'Worker'
                             ? CupertinoColors.activeBlue
                             : CupertinoColors.lightBackgroundGray,
                         borderRadius: const BorderRadius.only(
@@ -118,11 +118,11 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                       child: Text(
-                        'Crew',
+                        'Worker',
                         style: TextStyle(
-                          fontSize: 50, // 增加字體大小
+                          fontSize: 30, // 增加字體大小
                           fontWeight: FontWeight.bold,
-                          color: selectedRole == 'Crew'
+                          color: selectedRole == 'Worker'
                               ? CupertinoColors.white
                               : CupertinoColors.black,
                         ),
@@ -131,42 +131,42 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ],
               ),
-              const SizedBox(height: 50), //與上方元件的距離
+              const SizedBox(height: 35), //與上方元件的距離
               // 用戶名輸入框
               SizedBox(
-                height: 100,
-                width: 750,
+                height: 70,
+                width: 500,
                 child: CupertinoTextField(
                   placeholder: 'Username',
                   padding: const EdgeInsets.symmetric(
-                      vertical: 15, horizontal: 40), // 調整內邊距
+                      vertical: 5, horizontal: 20), // 調整內邊距
                   decoration: BoxDecoration(
                     color: CupertinoColors.lightBackgroundGray,
                     borderRadius: BorderRadius.circular(80),
                   ),
-                  style: const TextStyle(fontSize: 50), // 增加輸入框文字大小
+                  style: const TextStyle(fontSize: 30), // 增加輸入框文字大小
                 ),
               ),
 
               const SizedBox(height: 15), //與上方元件的距離
               // 密碼輸入框
               SizedBox(
-                height: 100,
-                width: 750,
+                height: 70,
+                width: 500,
                 child: CupertinoTextField(
                   placeholder: 'Password',
                   padding: const EdgeInsets.symmetric(
-                      vertical: 15, horizontal: 40), // 調整內邊距
+                      vertical: 5, horizontal: 20), // 調整內邊距
                   obscureText: true, // 啟用密碼遮罩功能
                   decoration: BoxDecoration(
                     color: CupertinoColors.lightBackgroundGray,
                     borderRadius: BorderRadius.circular(80),
                   ),
-                  style: const TextStyle(fontSize: 50), // 增加輸入框文字大小
+                  style: const TextStyle(fontSize: 30), // 增加輸入框文字大小
                 ),
               ),
               // 登錄按鈕
-              const SizedBox(height: 37),
+              const SizedBox(height: 20),
               SizedBox(
                 width: 400,
                 child: CupertinoButton(
@@ -177,7 +177,7 @@ class _LoginPageState extends State<LoginPage> {
                   child: const Text(
                     'LOGIN',
                     style: TextStyle(
-                      fontSize: 50, // 修改點：增加字體大小
+                      fontSize: 30, // 修改點：增加字體大小
                       fontWeight: FontWeight.bold,
                     ),
                   ),
