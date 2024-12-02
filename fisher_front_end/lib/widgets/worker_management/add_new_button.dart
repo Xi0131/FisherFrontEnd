97406190a2007a64933dfb6f46759cc10f21d458
Kiki768 <114562955+Kiki768@users.noreply.github.com>
2024-12-02 17:10:33 +0800

    Update add_new_button.dart

--- fisher_front_end/lib/widgets/worker_management/add_new_button.dart
+++ fisher_front_end/lib/widgets/worker_management/add_new_button.dart
@@ -1,70 +1,35 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
 
-// 人員卡片
-class PersonnelCard extends StatelessWidget {
-  final String name;
-  final String number;
-  final String image; // 假設這是員工圖片的路徑或 URL
+// 新增按鈕
+class AddNewButton extends StatelessWidget {
   final VoidCallback onTap;
 
-  const PersonnelCard({
-    super.key,
-    required this.name,
-    required this.number,
-    required this.image,
-    required this.onTap,
-  });
+  const AddNewButton({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return CupertinoButton(
       onPressed: onTap,
       child: Container(
+        width: 300,
         decoration: BoxDecoration(
           color: CupertinoColors.systemGrey6,
           borderRadius: BorderRadius.circular(20),
         ),
         padding: const EdgeInsets.all(15),
-        child: Column(
+        child: const Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 300, //長方形大小
-              height: 110, //圓形大小
-              decoration: const BoxDecoration(
+            Icon(
+              CupertinoIcons.add_circled,
+              size: 120,
+              color: CupertinoColors.activeBlue,
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Add New",
+              style: TextStyle(
                 color: CupertinoColors.activeBlue,
-                shape: BoxShape.circle,
-              ),
-              child: image.isNotEmpty
-                  ? ClipOval(
-                      child: Image.file(
-                        File(image), // 顯示本地圖片
-                        fit: BoxFit.cover,
-                        width: 300,
-                        height: 110,
-                      ),
-                    )
-                  : const Icon(
-                      CupertinoIcons.person,
-                      size: 70,
-                      color: CupertinoColors.white,
-                    ),
-            ),
-            const SizedBox(height: 5),
-            Text(
-              name,
-              style: const TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: CupertinoColors.black,
-              ),
-            ),
-            Text(
-              "No. $number",
-              style: const TextStyle(
-                color: CupertinoColors.systemGrey,
                 fontSize: 30,
               ),
             ),
@@ -74,4 +39,3 @@ class PersonnelCard extends StatelessWidget {
     );
   }
 }
-
