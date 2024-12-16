9e2a5f49839db0be7ce176415f009bb2ac33703b
Kiki768 <kiki5551801@gmail.com>
2024-12-16 14:27:15 +0800

    login page with api

--- fisher_front_end/lib/views/login_page.dart
+++ fisher_front_end/lib/views/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,27 +10,87 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // 用來跟蹤選擇的是 Captain 還是 Crew
+  // 使用者角色與輸入框變數
   String selectedRole = 'Captain'; // 預設選擇 Captain
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // 登錄按鈕邏輯
+  Future<void> _login() async {
+    final String username = usernameController.text;
+    final String password = passwordController.text;
+
+    // API 請求設定
+    final url = Uri.parse('http://35.229.208.250:3000/api/loginPage/login');
+    final body = jsonEncode({
+      'username': username,
+      'pattern': password,
+    });
+
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+
+      if (!mounted) return; // 確保 Widget 仍然存在
+
+      if (response.statusCode == 200) {
+        // final result = jsonDecode(response.body);
+        // print('Login Success: $result');
+        // 成功登入後導航至對應頁面
+        if (selectedRole == 'Captain') {
+          Navigator.pushNamed(context, 'captainPage');
+        } else {
+          Navigator.pushNamed(context, 'crewPage');
+        }
+      } else {
+        _showErrorDialog('Invalid username or password.');
+      }
+    } catch (e) {
+      if (!mounted) return; // 再次檢查 Widget 是否存在
+      // print('Error: $e');
+      _showErrorDialog('An error occurred. Please try again.');
+    }
+  }
+
+  // 顯示錯誤訊息的彈窗
+  void _showErrorDialog(String message) {
+    showCupertinoDialog(
+      context: context,
+      builder: (_) => CupertinoAlertDialog(
+        title: const Text('Login Failed'),
+        content: Text(message),
+        actions: [
+          CupertinoDialogAction(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context, rootNavigator: true).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      backgroundColor: const Color(0xFFF5F5F5), // 背景顏色
+      backgroundColor: const Color(0xFFF5F5F5),
       child: Center(
         child: Container(
-          padding: const EdgeInsets.all(20), // padding: 與邊邊留白的距離
-          width: 500, // 邊框長度
+          padding: const EdgeInsets.all(20),
+          width: 500,
           height: 500,
-
           decoration: BoxDecoration(
             color: CupertinoColors.white,
-            borderRadius: BorderRadius.circular(40), // 圓角
+            borderRadius: BorderRadius.circular(40),
             boxShadow: [
               BoxShadow(
-                color: CupertinoColors.black.withOpacity(0.1), //陰影特效
-                blurRadius: 15, //陰影模糊半徑
-                spreadRadius: 8, //陰影擴散半徑
+                color: CupertinoColors.black.withOpacity(0.1),
+                blurRadius: 15,
+                spreadRadius: 8,
               ),
             ],
           ),
@@ -44,139 +106,33 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               ),
               const SizedBox(height: 20),
-              // Captain 和 Crew 選擇按鈕
+              // Captain 和 Worker 選擇按鈕
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // Captain 按鈕
-                  CupertinoButton(
-                    padding: EdgeInsets.zero, // 移除 CupertinoButton 的默認內邊距
-                    onPressed: () {
-                      setState(() {
-                        selectedRole = 'Captain';
-                      });
-                    },
-
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        // 按鈕內的邊距（上下 15 像素，左右 25 像素），使文字有足夠的空間
-                        vertical: 15,
-                        horizontal: 20,
-                      ),
-                      decoration: BoxDecoration(
-                        // 控制外觀
-                        color: selectedRole == 'Captain'
-                            ? CupertinoColors.activeBlue // 選中為blue
-                            : CupertinoColors.lightBackgroundGray, // 否則為灰色
-                        borderRadius: const BorderRadius.only(
-                          // 只圓角左上角跟左下角
-                          topLeft: Radius.circular(100),
-                          bottomLeft: Radius.circular(100),
-                        ),
-                      ),
-                      child: Text(
-                        'Captain',
-                        style: TextStyle(
-                          fontSize: 30, // 增加字體大小
-                          fontWeight: FontWeight.bold, //文字加粗
-                          color: selectedRole ==
-                                  'Captain' //如果是選captain的話，文字為白色，否則為黑色
-                              ? CupertinoColors.white
-                              : CupertinoColors.black,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  // Crew 按鈕
-
-                  CupertinoButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      setState(() {
-                        selectedRole = 'Worker';
-                      });
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 15,
-                        horizontal: 25,
-                      ),
-                      decoration: BoxDecoration(
-                        color: selectedRole == 'Worker'
-                            ? CupertinoColors.activeBlue
-                            : CupertinoColors.lightBackgroundGray,
-                        borderRadius: const BorderRadius.only(
-                          topRight: Radius.circular(100),
-                          bottomRight: Radius.circular(100),
-                        ),
-                      ),
-                      child: Text(
-                        'Worker',
-                        style: TextStyle(
-                          fontSize: 30, // 增加字體大小
-                          fontWeight: FontWeight.bold,
-                          color: selectedRole == 'Worker'
-                              ? CupertinoColors.white
-                              : CupertinoColors.black,
-                        ),
-                      ),
-                    ),
-                  ),
+                  _roleButton('Captain'),
+                  _roleButton('Worker'),
                 ],
               ),
-              const SizedBox(height: 35), //與上方元件的距離
+              const SizedBox(height: 35),
               // 用戶名輸入框
-              SizedBox(
-                height: 70,
-                width: 500,
-                child: CupertinoTextField(
-                  placeholder: 'Username',
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 5, horizontal: 20), // 調整內邊距
-                  decoration: BoxDecoration(
-                    color: CupertinoColors.lightBackgroundGray,
-                    borderRadius: BorderRadius.circular(80),
-                  ),
-                  style: const TextStyle(fontSize: 30), // 增加輸入框文字大小
-                ),
-              ),
-
-              const SizedBox(height: 15), //與上方元件的距離
+              _inputField('Username', usernameController, false),
+              const SizedBox(height: 15),
               // 密碼輸入框
-              SizedBox(
-                height: 70,
-                width: 500,
-                child: CupertinoTextField(
-                  placeholder: 'Password',
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 5, horizontal: 20), // 調整內邊距
-                  obscureText: true, // 啟用密碼遮罩功能
-                  decoration: BoxDecoration(
-                    color: CupertinoColors.lightBackgroundGray,
-                    borderRadius: BorderRadius.circular(80),
-                  ),
-                  style: const TextStyle(fontSize: 30), // 增加輸入框文字大小
-                ),
-              ),
-              // 登錄按鈕
+              _inputField('Password', passwordController, true),
               const SizedBox(height: 20),
               SizedBox(
-                width: 400,
-                child: CupertinoButton(
-                  onPressed: () {
-                    // 按下按鈕的行為
-                    if (selectedRole == 'Captain') {
-                      Navigator.pushNamed(context, 'captainPage');
-                    } else {
-                      Navigator.pushNamed(context, 'crewPage');
-                    }
-                  },
-                  child: const Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      fontSize: 30, // 修改點：增加字體大小
-                      fontWeight: FontWeight.bold,
+                width: 250,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(50)), // 設定圓角
+                  child: CupertinoButton.filled(
+                    onPressed: _login,
+                    child: const Text(
+                      'LOGIN',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
@@ -184,6 +140,67 @@ class _LoginPageState extends State<LoginPage> {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  // 建立 Captain 和 Worker 選擇按鈕
+  Widget _roleButton(String role) {
+    return CupertinoButton(
+      padding: EdgeInsets.zero,
+      onPressed: () => setState(() => selectedRole = role),
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 25),
+        decoration: BoxDecoration(
+          color: selectedRole == role
+              ? CupertinoColors.activeBlue
+              : CupertinoColors.lightBackgroundGray,
+          borderRadius: BorderRadius.only(
+            topLeft: role == 'Captain'
+                ? const Radius.circular(100)
+                : Radius.zero,
+            bottomLeft: role == 'Captain'
+                ? const Radius.circular(100)
+                : Radius.zero,
+            topRight: role == 'Worker'
+                ? const Radius.circular(100)
+                : Radius.zero,
+            bottomRight: role == 'Worker'
+                ? const Radius.circular(100)
+                : Radius.zero,
+          ),
+        ),
+        child: Text(
+          role,
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: selectedRole == role
+                ? CupertinoColors.white
+                : CupertinoColors.black,
+          ),
+        ),
+      ),
+    );
+  }
+
+  // 建立輸入框
+  Widget _inputField(
+      String placeholder, TextEditingController controller, bool obscureText) {
+    return SizedBox(
+      height: 70,
+      width: 500,
+      child: CupertinoTextField(
+        controller: controller,
+        placeholder: placeholder,
+        padding:
+        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+        obscureText: obscureText,
+        decoration: BoxDecoration(
+          color: CupertinoColors.lightBackgroundGray,
+          borderRadius: BorderRadius.circular(80),
+        ),
+        style: const TextStyle(fontSize: 30),
       ),
     );
   }
