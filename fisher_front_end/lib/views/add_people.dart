f452a2a5f6f6b86ecffed9d21868f227a36a1398
Kiki768 <kiki5551801@gmail.com>
2024-12-17 23:26:18 +0800

    add_people with api

--- fisher_front_end/lib/views/add_people.dart
+++ fisher_front_end/lib/views/add_people.dart
@@ -1,6 +1,8 @@
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AddPersonnelPage extends StatefulWidget {
   final Function(Map<String, String>) onAdd; // 用於回調新增的資料
@@ -18,7 +20,6 @@ class AddPersonnelPage extends StatefulWidget {
 
 class AddPersonnelPageState extends State<AddPersonnelPage> {
   late TextEditingController nameController;
-  late TextEditingController numberController;
   late TextEditingController passportController;
   late TextEditingController countryController;
   late TextEditingController ageController;
@@ -41,17 +42,15 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
   String selectedWorkType = "Select Work Type";
   final List<String> workTypes = [
     "Select Work Type",
-    "Fisherman",
-    "Deckhands",
-    "FishProcessors",
-    "Engineers",
-    "Chef"
+    "fisherman",
+    "deckhand",
+    "fish processor",
+    "engineer",
+    "chef"
   ];
 
   bool isNameEmpty = false;
-  bool isNumberEmpty = false;
   bool isWorkTypeEmpty = false;
-  bool isNumberDuplicate = false;
   bool isPasswordEmpty = false;
   bool isConfirmPasswordEmpty = false;
   bool isPasswordDifferent = false;
@@ -59,12 +58,12 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
   bool isPassportEmpty = false;
   bool isCountryEmpty = false;
   bool _obscureText = true;
+  bool isLoading = false; // 控制加載狀態
 
   @override
   void initState() {
     super.initState();
     nameController = TextEditingController();
-    numberController = TextEditingController();
     passportController = TextEditingController();
     countryController = TextEditingController();
     ageController = TextEditingController();
@@ -75,7 +74,6 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
   @override
   void dispose() {
     nameController.dispose();
-    numberController.dispose();
     passportController.dispose();
     countryController.dispose();
     ageController.dispose();
@@ -84,44 +82,66 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
     super.dispose();
   }
 
-  void validateAndAdd() {
+  void validateAndAdd() async {
     setState(() {
       isNameEmpty = nameController.text.trim().isEmpty;
-      isNumberEmpty = numberController.text.trim().isEmpty;
       isAgeEmpty = ageController.text.trim().isEmpty;
       isPassportEmpty = passportController.text.trim().isEmpty;
       isCountryEmpty = countryController.text.trim().isEmpty;
       isPasswordEmpty = passwordController.text.trim().isEmpty;
       isConfirmPasswordEmpty = confirmPasswordController.text.trim().isEmpty;
       isWorkTypeEmpty = selectedWorkType == "Select Work Type";
-      isNumberDuplicate = widget.existingNumbers.contains(numberController.text.trim());
       isPasswordDifferent = passwordController.text != confirmPasswordController.text;
     });
 
     if (!isNameEmpty &&
-        !isNumberEmpty &&
         !isAgeEmpty &&
         !isPassportEmpty &&
         !isCountryEmpty &&
         !isPasswordEmpty &&
         !isConfirmPasswordEmpty &&
         !isWorkTypeEmpty &&
-        !isNumberDuplicate &&
         !isPasswordDifferent) {
-      widget.onAdd({
+      setState(() {
+        isLoading = true; // 開始加載
+      });
+
+      var requestData = {
         "name": nameController.text,
-        "number": numberController.text,
-        "passport": passportController.text,
+        "passport_number": passportController.text,
         "country": countryController.text,
         "age": ageController.text,
-        "role": selectedWorkType,
-        "password": passwordController.text,
-        "confirmPassword": confirmPasswordController.text,
-        "image": _image?.path ?? "",
-      });
-      Navigator.pop(context);
+        "job_title": selectedWorkType,
+        "pattern": passwordController.text,
+        "profilePhoto": _image?.path ?? "",
+      };
+
+      try {
+        final response = await http.post(
+          Uri.parse('http://35.229.208.250:3000/api/workerEdit/add-worker'),
+          headers: {'Content-Type': 'application/json'},
+          body: json.encode(requestData),
+        );
+
+        if (response.statusCode == 200) {
+          if (mounted) {
+            widget.onAdd(requestData);
+            Navigator.pop(context);
+          }
+        } else {
+          // print('Error: ${response.statusCode}');
+        }
+      } catch (error) {
+        // print('Request failed: $error');
+      } finally {
+        setState(() {
+          isLoading = false; // 結束加載
+        });
+      }
     }
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -138,7 +158,13 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
           },
         ),
       ),
-      child: SingleChildScrollView(
+      child: isLoading
+          ? const Center(
+        child: CupertinoActivityIndicator(
+          radius: 20, // 加載指示器大小
+        ),
+      )
+      : SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(80),
           child: Column(
@@ -168,8 +194,8 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
               const SizedBox(height: 20),
               _buildTextField(controller: nameController, placeholder: "Name", isEmpty: isNameEmpty),
               const SizedBox(height: 10),
-              _buildTextField(controller: numberController, placeholder: "Number", isEmpty: isNumberEmpty, isDuplicate: isNumberDuplicate),
-              const SizedBox(height: 10),
+              // _buildTextField(controller: numberController, placeholder: "Number", isEmpty: isNumberEmpty, isDuplicate: isNumberDuplicate),
+              // const SizedBox(height: 10),
               _buildTextField(controller: ageController, placeholder: "Age", isEmpty: isAgeEmpty),
               const SizedBox(height: 10),
               _buildTextField(controller: passportController, placeholder: "Passport", isEmpty: isPassportEmpty),
@@ -247,7 +273,6 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
     required TextEditingController controller,
     required String placeholder,
     bool isEmpty = false,
-    bool isDuplicate = false,
     bool isDifferent = false,
     bool isPassword = false,
   }) {
@@ -266,7 +291,7 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             border: Border.all(
-              color: isEmpty || isDuplicate || isDifferent
+              color: isEmpty || isDifferent
                   ? CupertinoColors.destructiveRed
                   : CupertinoColors.lightBackgroundGray,
               width: 2,
@@ -300,17 +325,6 @@ class AddPersonnelPageState extends State<AddPersonnelPage> {
           )
               : null,
         ),
-        if (isDuplicate)
-          const Padding(
-            padding: EdgeInsets.only(left: 10, top: 5),
-            child: Text(
-              "This number is already in use.",
-              style: TextStyle(
-                color: CupertinoColors.destructiveRed,
-                fontSize: 16,
-              ),
-            ),
-          ),
         if (isDifferent)
           const Padding(
             padding: EdgeInsets.only(left: 10, top: 5),
